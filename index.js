6e0c5c5d5bfcc1d04b123c38c29273e2722c4fa0
cifer <cifergciferg@gmail.com>
2021-10-25 12:23:35 +0530

    make it

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ var express = require('express');
 var app = express();
 
 var requestIp = require('request-ip');
+var geoip = require('geoip-country');
 
 app.get('/',function(request, response) {
 
     var clientIp = requestIp.getClientIp(request);
+    var geo = geoip.lookup(clientIp);
     console.log(clientIp);
+    console.log(geo);
     response.send(clientIp)
 
 });
